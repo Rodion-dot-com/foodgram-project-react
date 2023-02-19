4810a84f12a8b30d339ee42de464611e008cb405
Родион Прошляков <proshlyakovrodion@yandex.ru>
2023-02-19 21:10:24 +0300

    Создана админка для приложения recipes

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,9 +1,38 @@
 from django.contrib import admin
 
-from .models import Recipe, Ingredient, Tag, IngredientRecipe, TagRecipe
+from .models import (Recipe, Ingredient, Tag, IngredientRecipe, TagRecipe,
+                     Favorites)
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(Tag)
-admin.site.register(IngredientRecipe)
-admin.site.register(TagRecipe)
+
+class IngredientInline(admin.TabularInline):
+    model = IngredientRecipe
+    extra = 1
+
+
+class TagInline(admin.TabularInline):
+    model = TagRecipe
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author',)
+    list_filter = ('name', 'author', 'tags')
+    inlines = (IngredientInline, TagInline,)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'unit_of_measurement',)
+    list_filter = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug',)
+
+
+@admin.register(Favorites)
+class FavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe',)
+    list_filter = ('user', 'recipe',)
--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -2,52 +2,91 @@ from django.db import models
 from django.core.validators import MinValueValidator
 from users.models import User
 
+MAX_CHAR_FIELD_SIZE = 200
+MAX_SLUG_FIELD_SIZE = 200
+
 
 class Tag(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, null=True)
+    name = models.CharField(
+        max_length=MAX_CHAR_FIELD_SIZE,
+        unique=True,
+        verbose_name='название',
+    )
+    slug = models.SlugField(
+        max_length=MAX_SLUG_FIELD_SIZE,
+        unique=True,
+        verbose_name='уникальный адрес группы',
+    )
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
 
     def __str__(self):
-        return f'{self.name} - {self.slug}'
+        return self.name
 
 
 class Ingredient(models.Model):
-    name = models.CharField(max_length=200)
-    unit_of_measurement = models.CharField(max_length=200)
+    name = models.CharField(
+        max_length=MAX_CHAR_FIELD_SIZE,
+        verbose_name='название',
+    )
+    unit_of_measurement = models.CharField(
+        max_length=MAX_CHAR_FIELD_SIZE,
+        verbose_name='единицы измерения',
+    )
+
+    class Meta:
+        unique_together = (
+            'name',
+            'unit_of_measurement',
+        )
+        verbose_name = 'ингредиент'
+        verbose_name_plural = 'ингредиенты'
 
     def __str__(self):
-        return f'{self.name} {self.unit_of_measurement}'
+        return self.name
 
 
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='recipes'
+        related_name='recipes',
+        verbose_name='автор публикации',
     )
     name = models.CharField(
-        max_length=200,
-        blank=False,
-        null=False
+        max_length=MAX_CHAR_FIELD_SIZE,
+        verbose_name='название',
     )
     text = models.TextField(
-        blank=False,
-        null=False,
+        verbose_name='текстовое описание',
     )
     cooking_time = models.PositiveSmallIntegerField(
-        validators=(MinValueValidator(1),)
+        validators=(MinValueValidator(1),),
+        verbose_name='время приготовления в минутах',
     )
     image = models.ImageField(
-        upload_to='recipes/'
+        upload_to='recipes/',
+        null=True,
+        verbose_name='картинка',
     )
     ingredients = models.ManyToManyField(
         Ingredient,
-        through='IngredientRecipe'
+        through='IngredientRecipe',
     )
     tags = models.ManyToManyField(
         Tag,
-        through='TagRecipe'
+        through='TagRecipe',
     )
+    liked_users = models.ManyToManyField(
+        User,
+        through='Favorites',
+    )
+
+    class Meta:
+        verbose_name = 'рецепт'
+        verbose_name_plural = 'рецепты'
 
     def __str__(self):
         return self.name
@@ -56,32 +95,74 @@ class Recipe(models.Model):
 class IngredientRecipe(models.Model):
     ingredient = models.ForeignKey(
         Ingredient,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='ингредиент'
     )
     recipe = models.ForeignKey(
         Recipe,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='рецепт',
     )
-    count = models.PositiveSmallIntegerField(
-        validators=(MinValueValidator(1),)
+    quantity = models.FloatField(
+        validators=(MinValueValidator(0),),
+        verbose_name='количество',
     )
+
+    class Meta:
+        verbose_name = 'входящий ингредиент'
+        verbose_name_plural = 'входящие ингредиенты'
 
     def __str__(self):
         return (
-            f'В рецепте {self.recipe.name}: {self.ingredient.name} - '
-            f'{self.count} {self.ingredient.unit_of_measurement}'
+            f'{self.ingredient.name} - {self.quantity} '
+            f'{self.ingredient.unit_of_measurement}'
         )
 
 
 class TagRecipe(models.Model):
     tag = models.ForeignKey(
         Tag,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='тег',
     )
     recipe = models.ForeignKey(
         Recipe,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        verbose_name='рецепт',
     )
 
+    class Meta:
+        unique_together = (
+            'tag',
+            'recipe',
+        )
+        verbose_name = 'указанный тег'
+        verbose_name_plural = 'указанные теги'
+
     def __str__(self):
-        return f'{self.recipe.name} [{self.tag.name}]'
+        return f'{self.recipe} #{self.tag.slug}'
+
+
+class Favorites(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='рецепт'
+    )
+
+    class Meta:
+        unique_together = (
+            'user',
+            'recipe',
+        )
+        verbose_name = 'избранный рецепт'
+        verbose_name_plural = 'избранные рецепты'
+
+    def __str__(self):
+        return f'{self.recipe} в избранном у {self.user}'
+
